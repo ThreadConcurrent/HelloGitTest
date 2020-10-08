4d59d94f84dae500ddc96462ebf6f238ecfc8233
tom_glb <tom_global@163.com>
2020-10-08 17:03:51 +0800

    创建分支

--- hello-git-01/src/main/java/com/wangcheng/git/HelloGit.java
+++ hello-git-01/src/main/java/com/wangcheng/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit");
         System.out.println("HelloGit2");
+        System.out.println("创建分支");
     }
 }

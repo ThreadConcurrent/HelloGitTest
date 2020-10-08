81c9c0b7c28443ce71abf190e97b9100640ee7a6
tom_glb <tom_global@163.com>
2020-10-08 16:40:47 +0800

    更新1

--- hello-git-01/src/main/java/com/wangcheng/git/HelloGit.java
+++ hello-git-01/src/main/java/com/wangcheng/git/HelloGit.java
@@ -8,5 +8,6 @@ package com.wangcheng.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+        System.out.println("HelloGit2");
     }
 }

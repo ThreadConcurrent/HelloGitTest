b7994170a318a35089c41786f0743c84f35cd28b
tom_glb <tom_global@163.com>
2020-10-08 16:39:16 +0800

    添加文件

---
+++ hello-git-01/src/main/java/com/wangcheng/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.wangcheng.git;
+
+/**
+ * @author WangCheng
+ * @describe
+ * @create 2020-10-07 12:56
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
